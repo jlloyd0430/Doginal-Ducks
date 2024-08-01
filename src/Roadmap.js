9893739dd563484dd3b8f77abe8c0d092e2524ed
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-08-01 14:54:04 -0400

    Update Roadmap.js

--- src/Roadmap.js
+++ src/Roadmap.js
@@ -8,8 +8,17 @@ const Roadmap = () => {
     { title: 'Brainstorm', description: 'Get to know the community, release socials, Find out what the ecosystem needs or is lacking.' },
     { title: 'Dogepond.com', description: 'Build.First we start with Dogepond.com The first doginal event calendar platform. A place where users can post  there upcoming projects and gain exposure before there mint, also allowing other people to see whats coming up in the ecosystem,post are posted in parallel on X and by our bot in many servers,free to post , free marketing.' },
     { title: 'Discord Tools', description: 'We decided Doge was in need of many tools, so we build a toolbelt of various tools and offer them to anyone who might be in need as a service of dogepond. Holder verification, sales, wallet tracking, trending volume, dunes tracking, and so many other bots. ' },
-    { title: 'Minting Tech', description: 'Doginals need a better minting experience plain and simple, we plan to deliver a mint and recieve automatic traditional minting service to DOGE.' },
-    { title: 'Governance', description: 'Doge is a very community and culture oriented blockchain, we are creating governance and voting solutions for doginals, Drc-20 tokens, and Dunes we want to build a reliable governance system for everyone to utilize ' },
+    { title: 'DING', description: 'Create a Strong Utility for the Doginal Ducks, introducing DING the first wallet tracker on doge.' },
+    { title: 'Minting Tech', description: 'Doginals need a better minting experience plain and simple, we plan to deliver a mint and recieve automatic traditional minting service to DOGE. We will continue to innovate and build more and more unique and diverse minting tech for the space to launch with.' },
+    { title: 'Governance', description: 'Doge is a very community and culture oriented blockchain, we believe creating governance and voting solutions for doginals will be a very valuble use case for DAO based or community oriented projects,We are building governance for Doginals, Drc-20 tokens, and Dunes we want to build a reliable governance system for everyone to utilize on all fronts. ' },
+    { title: 'Dogepond upgrades', description: 'Add more market data, analytics and tools to dogepond, aswell as improve ui/ux wherever possible,' },
+    { title: 'Upgrade Node', description: 'Setup a dogecoin node through VPS so we can offer much larger services and open up opportunities for us to build alot more things.' },
+    { title: 'Rare Evo', description: 'Doginals is just over a year old, still not many people even know that we exist! but MANY people know DOGE, we will be attending Rare evo an irl multi-chain event this year in Las Vegas, this will be a great networking opportunity for us and also give the whole ecosystem exposure' },
+    { title: 'Doginal Ducks expansion', description: 'Demonstrating and exercising the power of our new minting tech we will release a new and improved 2d collection of ducks on DOGE to now allow more people easier access to our tools and ecosystem. We will still hold exclusive utilities that weve always had for OG Doginal duck holders like Dingbot Pro, and other free and discounted benefits around our services.' },
+    { title: 'Dogepond v2', description: 'More TBA.' },
+    { title: 'Market?', description: 'stay tuned, we wont dissapoint.' },
+ 
+    
   ];
 
   return (
